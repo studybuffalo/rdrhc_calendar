badc26bf6d78c9b43112cc9a378bb63f4d238a22
Joshua Torrance <torrancj@gmail.com>
2017-10-07 16:25:26 -0600

    Finalizing initial update to make use of template emails for user updates

--- modules/notify.py
+++ modules/notify.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import logging
+import re
 import smtplib
 
 # Setup logger
@@ -72,15 +73,6 @@ def email_schedule(user, config, schedule):
     if (len(schedule.additions) or len(schedule.deletions) or 
         len(schedule.changes) or len(schedule.missing) or len(schedule.null)):
         
-        fromEmail = config["email"]["from_email"]
-        toEmail = user.email
-        subject = "RDRHC Schedule Changes"
-        
-        content = MIMEMultipart('alternative')
-        content['From'] = fromEmail
-        content['To'] = toEmail
-        content['Subject'] = subject
-        
         # Opens the update email (text) file
         text_loc = config["email"]["update_text"]
         
@@ -99,153 +91,167 @@ def email_schedule(user, config, schedule):
         except Exception as e:
             log.exception("Unable to read welcome email html template")
         
-        # Initial HTML Setup
-        html.append("<html>")
-        html.append("<head></head>")
-        html.append("<body>")
-        
-        # Email opening
-
-        html.append("<p>Hello %s,</p>" % user.name)
-        html.append("<p>Please see the following details regarding your "
-                    "work schedule at the Red Deer Regional Hospital "
-                    "Centre:</p>")
-        
-        # Cycle through changes and create html & text email messages
+        # Manage added shifts
         if len(schedule.additions):
-           
-            html.append("<b>ADDITIONS</b>")
-            html.append("<ul>")
-            
-            for a in schedule.additions:
-                text.append(a.msg)
-                
-                html.append("<li>%s</li>" % a.msg)
-                
-            text.append("")
-            
-            html.append("</ul>")
-            
-        if len(schedule.deletions):
-            
-            
-            html.append("<b>DELETIONS</b>")
-            html.append("<ul>")
-            
-            for d in schedule.deletions:
-                text.append(d.msg)
-                
-                html.append("<li>%s</li>" % d.msg)
-                
-            text.append("")
+            # Cycle through additions and insert into templates
+            additions_text = []
+            additions_html = []
 
-            html.append("</ul>")
+            for a in schedule.additions:
+                additions_text.append(" - {}".format_map(a.msg))
+                additions_html.append("<li>{}</li>".format(a.msg))
+
+            text = text.replace("{{ additions }}", "\r\n".join(additions_text))
+            html = html.replace("{{ additions }}", "".join(additions_html))
+
+            # Remove the block markers
+            text = text.replace("{% block additions %}", "")
+            html = html.replace("{% block additions %}", "")
+        else:
+            # No additions, remove entire section
+            regex = r"{% block additions %}.*{% block additions %}"
+
+            text = re.sub(regex, "", text, flags=re.S)
+            html = re.sub(regex, "", html, flags=re.S)
+
+        # Manage deleted shifts
+        if len(schedule.deletions):
+            deletions_text = []
+            deletions_html = []
+
+            for d in schedule.deletions:
+                deletions_text.append(" - {}".format_map(d.msg))
+                deletions_html.append("<li>{}</li>".format(d.msg))
+                
+            text.replace("{{ deletions }}", "\r\n".join(deletions_text))
+            html.replace("{{ deletions }}", "".join(deledtions_html))
+
+            # Remove the block markers
+            text = text.replace("{% block deletions %}", "")
+            html = html.replace("{% block deletions %}", "")
+        else:
+            # No deletions, remove entire section
+            regex = r"{% block deletions %}.*{% block deletions %}"
             
+            text = re.sub(regex, "", text, re.S)
+            html = re.sub(regex, "", html, re.S)
+
+        # Manage changed shifts
         if len(schedule.changes):
-            
-            
-            html.append("<b>CHANGES</b>")
-            html.append("<ul>")
-            
+            changes_text = []
+            changes_html = []
+
             for c in schedule.changes:
-                text.append(c.msg)
+                changes_text.append(" - {}".format_map(c.msg))
+                changes_html.append("<li>{}</li>".format(c.msg))
                 
-                html.append("<li>%s</li>" % c.msg)
-                
-            text.append("")
+            text.replace("{{ deletions }}", "\r\n".join(changes_text))
+            html.replace("{{ deletions }}", "".join(changes_html))
+
+            # Remove the block markers
+            text = text.replace("{% block changes %}", "")
+            html = html.replace("{% block changes %}", "")
+        else:
+            # No changes, remove entire section
+            regex = r"{% block changes %}.*{% block changes %}"
             
-            html.append("</ul>")
-            
+            text = re.sub(regex, "", text, re.S)
+            html = re.sub(regex, "", html, re.S)
+
+        # Manage missing shifts
         if len(schedule.missing):
             defaults = config["calendar_defaults"]
-            format(
-                defaults["weekday_start"].strftime("%H:%M"),
-                defaults["weekday_end"].strftime("%H:%M"),
-                defaults["weekend_start"].strftime("%H:%M"),
-                defaults["weekend_end"].strftime("%H:%M"),
-                defaults["stat_start"].strftime("%H:%M"),
-                defaults["stat_end"].strftime("%H:%M"),
-            )
-            
-            html.append("<b>MISSING SHIFT CODES</b>")
-            html.append(("<br><i>A default shift time of {} to {} (weekdays), "
-                         "{} to {} (weekends), or {} to {} (statutory " 
-                         "holidays) has been used for these shifts</i>").format(
-                            defaults["weekday_start"].strftime("%H:%M"),
-                            defaults["weekday_end"].strftime("%H:%M"),
-                            defaults["weekend_start"].strftime("%H:%M"),
-                            defaults["weekend_end"].strftime("%H:%M"),
-                            defaults["stat_start"].strftime("%H:%M"),
-                            defaults["stat_end"].strftime("%H:%M"),
-                        ))
-            html.append("<ul>")
-            
+
+            weekday_start = defaults["weekday_start"].strftime("%H:%M")
+            text = text.replace("{{ weekday_start }}", weekday_start)
+            html = html.replace(" {{ weekday_start }}", weekday_start)
+
+            weekday_end = defaults["weekday_end"].strftime("%H:%M")
+            text = text.replace("{{ weekday_end }}", weekday_end)
+            html = html.replace(" {{ weekday_end}}", weekday_end)
+
+            weekend_start = defaults["weekend_start"].strftime("%H:%M")
+            text = text.replace("{{ weekend_start }}", weekend_start)
+            html = html.replace(" {{ weekend_start }}", weekend_start)
+
+            weekend_end = defaults["weekend_end"].strftime("%H:%M")
+            text = text.replace("{{ weekend_end }}", weekend_end)
+            html = html.replace(" {{ weekend_end}}", weekend_end)
+
+            stat_start = defaults["stat_start"].strftime("%H:%M")
+            text = text.replace("{{ stat_start }}", stat_start)
+            html = html.replace(" {{ stat_start }}", stat_start)
+
+            stat_end = defaults["stat_end"].strftime("%H:%M")
+            text = text.replace("{{ stat_end }}", stat_end)
+            html = html.replace(" {{ stat_end}}", stat_end)
+
+            missing_text = []
+            missing_html = []
+
             for m in schedule.missing:
-                text.append(m.msg)
+                missing_text.append(" - {}".format_map(m.msg))
+                missing_html.append("<li>{}</li>".format(m.msg))
                 
-                html.append("<li>%s</li>" % m.msg)
-                
-            text.append("")
+            text.replace("{{ missing }}", "\r\n".join(missing_text))
+            html.replace("{{ missing }}", "".join(missing_html))
+
+            # Remove the block markers
+            text = text.replace("{% block missing %}", "")
+            html = html.replace("{% block missing %}", "")
+        else:
+            # No missing shifts, remove entire section
+            regex = r"{% block missing %}.*{% block missing %}"
             
-            html.append("</ul>")
-            
+            text = re.sub(regex, "", text, re.S)
+            html = re.sub(regex, "", html, re.S)
+
+        # Manage excluded shifts
         if len(schedule.null):
-            
-            
-            html.append("<b>EXCLUDED CODES</b>")
-            html.append("<br><i>These codes are for you to review to "
-                        "ensure no work shifts have been missed; these "
-                        "codes have interpretted either as holidays/"
-                        "vacations/sick time/etc. or as being unrelated to "
-                        "start and end times</i>")
-            html.append("<ul>")
-            
+            null_text = []
+            null_html = []
+
             for n in schedule.null:
-                text.append(n.msg)
+                null_text.append(" - {}".format_map(n.msg))
+                null_html.append("<li>{}</li>".format(n.msg))
                 
-                html.append("<li>%s</li>" % n.msg)
-                
-            text.append("")
+            text.replace("{{ excluded }}", "\r\n".join(null_text))
+            html.replace("{{ excluded }}", "".join(null_html))
+
+            # Remove the block markers
+            text = text.replace("{% block excluded %}", "")
+            html = html.replace("{% block excluded %}", "")
+        else:
+            # No excluded shifts, remove entire section
+            regex = r"{% block excluded %}.*{% block excluded %}"
             
-            html.append("</ul>")
-            
-        # Add email footer
-        # Generate File Name
-        fileName = user.calendar_name
+            text = re.sub(regex, "", text, re.S)
+            html = re.sub(regex, "", html, re.S)
 
-        # Generate File Location
-        fileLoc = fileName
+        # Add the calendar name
+        calendar_name = user.calendar_name
+        text = text.replace("{{ calendar_name }}", calendar_name)
+        html = html.replace("{{ calendar_name }}", calendar_name)
 
-        html.append("<hr>")
-        html.append("<p>The address for your schedule is: "
-                    "http://www.studybuffalo.com/calendar/%s.ics </p>" 
-                    % fileLoc)
         
-        # Add tutorials
-        html.append("<p>For help using the calendar file, please see the "
-                    "tutorials located at: "
-                    "http://www.studybuffalo.com/calendar/ </p>")
+        # Setup email settings
+        fromEmail = config["email"]["from_email"]
+        toEmail = user.email
+        subject = "RDRHC Schedule Changes"
         
-        # Add final notice
+        content = MIMEMultipart('alternative')
+        content['From'] = fromEmail
+        content['To'] = toEmail
+        content['Subject'] = subject
         
-        
-        html.append("<p>If you wish to have any modifications made to your "
-                    "shift codes (including exlcuded or missing shift "
-                    "codes), please contact the owner of this program</p>")
-        
-		# HTML finishing
-        html.append("</body>")
-        html.append("</html>")
-
-        # Assemble final html and txt email content
-        html = "".join(html)
-        
+        # Construct the email body
         textBody = MIMEText(text, 'plain')
         htmlBody = MIMEText(html, 'html')
         
         content.attach(textBody)
         content.attach(htmlBody)
         
+        # Send the email
         try:
             if config["debug"]["email_console"]:
                 log.debug(content.as_string())
