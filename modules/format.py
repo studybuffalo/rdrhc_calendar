1a887cdb8fba33e1b462a18160c225e8e5ae7c90
studybuffalo <studybuffalo@gmail.com>
2018-05-17 21:04:59 -0600

    Retrying new code to handle blank shift codes

--- modules/format.py
+++ modules/format.py
@@ -363,13 +363,7 @@ def extract_raw_schedule(book, sheet, user, index, row_start, row_end, date_col)
             comment = ""
 
         # Add shift to master list if it has a date and shift code
-        print("Search Test")
-        print(re.search(r"^\s+$", shift_codes))
-
-        print("Match Test")
-        print(re.match(r"\s+$", shift_codes))
-
-        if shift_codes != "" and date != "":
+        if shift_codes != "" and re.match(r"\s+$", shift_codes) is None and date != "":
             # Split each shift code on spaces or slashes
             shift_codes = re.split(r"(?:\s|/)+", shift_codes)
             
