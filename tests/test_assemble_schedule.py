ebbe2daba43cf164b663a43f345cb593f0250904
studybuffalo <studybuffalo@gmail.com>
2018-10-17 20:07:37 -0600

    Updating tests to accomodate the new Schedule object

--- tests/test_assemble_schedule.py
+++ tests/test_assemble_schedule.py
@@ -1,4 +1,5 @@
 """Unit tests for the manager module."""
+# pylint: disable=protected-access
 
 from datetime import datetime, time
 from decimal import Decimal
@@ -12,7 +13,30 @@ from tests.utils import MockRequest404Response, MockRequest200Response
 
 APP_CONFIG = {
     'api_url': 'https://127.0.0.1/api/',
-    'api_headers': {'user-agent': 'rdrhc-calendar',}
+    'api_headers': {'user-agent': 'rdrhc-calendar',},
+}
+
+USER = {
+    'id': 1,
+}
+
+OLD_SCHEDULE = {
+    '2018-01-01': [
+        {"shift_code": "C1", "start_date": "2018-01-01"},
+    ],
+    '2018-02-01': [
+        {"shift_code": "C1", "start_date": "2018-02-01"},
+    ],
+    '2018-03-01': [
+        {"shift_code": "C1", "start_date": "2018-03-01"},
+    ],
+    '2018-04-01': [
+        {"shift_code": "C1", "start_date": "2018-04-01"},
+        {"shift_code": "F", "start_date": "2018-04-01"},
+    ],
+    '2018-05-01': [
+        {"shift_code": "C1", "start_date": "2018-12-01"},
+    ]
 }
 
 EXTRACTED_SCHEDULE = [
@@ -27,6 +51,21 @@ EXTRACTED_SCHEDULE = [
         'comment': '',
     },
 ]
+
+class MockRetrieveOldScheduleResponse(MockRequest200Response):
+    """A mock of a response to the retrieve_old_schedule function."""
+    def __init__(self, url, headers):
+        super().__init__(url, headers)
+        self.text = """[
+            {"text_shift_code": "C1", "date": "2018-01-01"},
+            {"text_shift_code": "C1", "date": "2018-02-01"},
+            {"text_shift_code": "X", "date": "2018-02-01"},
+            {"text_shift_code": "X", "date": "2018-03-01"},
+            {"text_shift_code": "C1", "date": "2018-03-01"},
+            {"text_shift_code": "C1", "date": "2018-04-01"},
+            {"text_shift_code": "F", "date": "2018-04-01"},
+            {"text_shift_code": "C1", "date": "2018-12-01"}
+        ]"""
 
 class MockRetrieveShiftCodeResponse(MockRequest200Response):
     """A mock of a response to the retireve_shift_codes function."""
@@ -54,77 +93,6 @@ class MockRetrieveStatHolidaysResponse(MockRequest200Response):
             "2018-07-01", "2018-08-06", "2018-09-03", "2018-10-08",
             "2018-11-11", "2018-12-25"
         ]"""
-
-class MockRetrieveOldScheduleResponse(MockRequest200Response):
-    """A mock of a response to the retrieve_old_schedule function."""
-    def __init__(self, url, headers):
-        super().__init__(url, headers)
-        self.text = """[
-            {"text_shift_code": "C1", "date": "2018-01-01"},
-            {"text_shift_code": "C1", "date": "2018-02-01"},
-            {"text_shift_code": "X", "date": "2018-02-01"},
-            {"text_shift_code": "X", "date": "2018-03-01"},
-            {"text_shift_code": "C1", "date": "2018-03-01"},
-            {"text_shift_code": "C1", "date": "2018-04-01"},
-            {"text_shift_code": "F", "date": "2018-04-01"},
-            {"text_shift_code": "C1", "date": "2018-12-01"}
-        ]"""
-
-@patch('requests.get', MockRequest404Response)
-def test_retrieve_shift_codes_404_response():
-    """Tests handling of 404 response in retrieve_shift_codes."""
-    try:
-        assemble_schedule.retrieve_shift_codes(APP_CONFIG, 1)
-    except ScheduleError:
-        assert True
-    else:
-        assert False
-
-@patch('requests.get', MockRetrieveShiftCodeResponse)
-def test_retrieve_shift_codes_time_conversions():
-    """Tests that time conversions work properly."""
-    shift_codes = assemble_schedule.retrieve_shift_codes(APP_CONFIG, 1)
-
-    assert isinstance(shift_codes[0]['monday_start'], time)
-
-@patch('requests.get', MockRetrieveShiftCodeResponse)
-def test_retrieve_shift_codes_decimal_conversions():
-    """Tests that decimal conversions work properly."""
-    shift_codes = assemble_schedule.retrieve_shift_codes(APP_CONFIG, 1)
-
-    assert isinstance(shift_codes[0]['monday_duration'], Decimal)
-
-@patch('requests.get', MockRequest404Response)
-def test_retrieve_stat_holidays_404_response():
-    """Tests handling of 404 response in retrieve_stat_holidays."""
-    try:
-        assemble_schedule.retrieve_stat_holidays(
-            APP_CONFIG, EXTRACTED_SCHEDULE
-        )
-    except ScheduleError:
-        assert True
-    else:
-        assert False
-
-@patch('requests.get', MockRetrieveStatHolidaysResponse)
-def test_retrieve_stat_holidays_date_conversion():
-    """Tests handling of empty schedule in retrieve_stat_holidays."""
-
-    stat_holidays = assemble_schedule.retrieve_stat_holidays(
-        APP_CONFIG, EXTRACTED_SCHEDULE
-    )
-
-    assert isinstance(stat_holidays[0], datetime)
-
-@patch('requests.get', MockRetrieveStatHolidaysResponse)
-def test_retrieve_stat_holidays_with_no_shifts():
-    """Tests handling of empty schedule in retrieve_stat_holidays."""
-
-    stat_holidays = assemble_schedule.retrieve_stat_holidays(
-        APP_CONFIG, []
-    )
-
-    assert len(stat_holidays) == 10
 
 @patch('requests.get', MockRequest404Response)
 def test_retrieve_old_schedule_404_response():
@@ -159,3 +127,69 @@ def test_retrieve_old_schedule_x_shift_handling():
                 x_removed = True
 
     assert x_removed
+
+@patch('requests.get', MockRequest404Response)
+def test_retrieve_shift_codes_404_response():
+    """Tests handling of 404 response in retrieve_shift_codes."""
+    try:
+        schedule = assemble_schedule.Schedule(
+            OLD_SCHEDULE, EXTRACTED_SCHEDULE, USER, APP_CONFIG
+        )
+        schedule._retrieve_shift_codes()
+    except ScheduleError:
+        assert True
+    else:
+        assert False
+
+@patch('requests.get', MockRetrieveShiftCodeResponse)
+def test_retrieve_shift_codes_time_conversions():
+    """Tests that time conversions work properly."""
+    schedule = assemble_schedule.Schedule(
+        OLD_SCHEDULE, EXTRACTED_SCHEDULE, USER, APP_CONFIG
+    )
+    shift_codes = schedule._retrieve_shift_codes()
+
+    assert isinstance(shift_codes[0]['monday_start'], time)
+
+@patch('requests.get', MockRetrieveShiftCodeResponse)
+def test_retrieve_shift_codes_decimal_conversions():
+    """Tests that decimal conversions work properly."""
+    schedule = assemble_schedule.Schedule(
+        OLD_SCHEDULE, EXTRACTED_SCHEDULE, USER, APP_CONFIG
+    )
+    shift_codes = schedule._retrieve_shift_codes()
+
+    assert isinstance(shift_codes[0]['monday_duration'], Decimal)
+
+@patch('requests.get', MockRequest404Response)
+def test_retrieve_stat_holidays_404_response():
+    """Tests handling of 404 response in retrieve_stat_holidays."""
+    try:
+        schedule = assemble_schedule.Schedule(
+            OLD_SCHEDULE, EXTRACTED_SCHEDULE, USER, APP_CONFIG
+        )
+        schedule._retrieve_stat_holidays()
+    except ScheduleError:
+        assert True
+    else:
+        assert False
+
+@patch('requests.get', MockRetrieveStatHolidaysResponse)
+def test_retrieve_stat_holidays_date_conversion():
+    """Tests handling of empty schedule in retrieve_stat_holidays."""
+    schedule = assemble_schedule.Schedule(
+        OLD_SCHEDULE, EXTRACTED_SCHEDULE, USER, APP_CONFIG
+    )
+    stat_holidays = schedule._retrieve_stat_holidays()
+
+    assert isinstance(stat_holidays[0], datetime)
+
+@patch('requests.get', MockRetrieveStatHolidaysResponse)
+def test_retrieve_stat_holidays_with_no_shifts():
+    """Tests handling of empty schedule in retrieve_stat_holidays."""
+    schedule = assemble_schedule.Schedule(
+        OLD_SCHEDULE, EXTRACTED_SCHEDULE, USER, APP_CONFIG
+    )
+    stat_holidays = schedule._retrieve_stat_holidays()
+
+    assert len(stat_holidays) == 10
