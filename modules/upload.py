0e0f4243ff7c18e60c931ea2b41bc47f7b1cf23d
studybuffalo <studybuffalo@gmail.com>
2018-10-20 09:46:15 -0600

    Creating the API inteface code for update_missing_codes_database

--- modules/upload.py
+++ modules/upload.py
@@ -9,7 +9,7 @@ import requests
 # Setup logger
 LOG = logging.getLogger(__name__)
 
-def delete_user_schedule(user_id, app_config):
+def delete_user_schedule(app_config, user_id):
     """Removes the provided users schedule from the database."""
     LOG.debug("Removing old shifts for user id = %s", user_id)
 
@@ -25,7 +25,7 @@ def delete_user_schedule(user_id, app_config):
             ).format(api_url)
         )
 
-def upload_user_schedule(user_id, schedule, app_config):
+def upload_user_schedule(app_config, user_id, schedule):
     """Uploads the provided users schedule to the database."""
     LOG.debug("Uploading the new shifts for user")
 
@@ -60,29 +60,31 @@ def upload_user_schedule(user_id, schedule, app_config):
 def update_schedule_database(user, schedule, app_config):
     """Uploads user schedule to Django Database"""
     # Delete the current schedule
-    delete_user_schedule(user['sb_user'], app_config)
+    delete_user_schedule(app_config, user['sb_user'])
 
     # Upload the new schedule
-    upload_user_schedule(user['sb_user'], schedule, app_config)
+    upload_user_schedule(app_config, user['sb_user'], schedule)
 
-def update_missing_codes_database(missing_codes):
+def update_missing_codes_database(app_config, missing_codes):
     """Uploads any new missing shift codes"""
     LOG.debug("Checking for missing shift codes")
 
-    new_codes = []
+    api_url = '{}shifts/missing/upload/'.format(app_config['api_url'])
 
-    # for role, code_set in missing_codes.items():
-    #     for code in code_set:
-    #         if code:
-                # retrieved_code, missing_code = MissingShiftCode.objects.get_or_create(
-                #     code=code,
-                #     role=role
-                # )
+    response = requests.post(
+        api_url,
+        data=json.dumps(missing_codes),
+        headers=app_config['api_headers'],
+    )
 
-                # # If this is a new code, record it to email owner
-                # if missing_code:
-                #     LOG.debug("New code to upload: {}".format(code))
+    if response.status_code >= 400:
+        raise requests.ConnectionError(
+            (
+                'Unable to connect to API ({}) and upload '
+                'missing codes: {}'
+            ).format(api_url, response.text)
+        )
 
-                #     new_codes.append("{} - {}".format(role, code))
+    new_codes = json.loads(response.text)
 
     return new_codes
