34494fb3d884b6ef407c47d2bec63bb1abed10c3
studybuffalo <studybuffalo@gmail.com>
2018-10-21 08:35:04 -0600

    Adding better exception handling for upload errors

--- modules/custom_exceptions.py
+++ modules/custom_exceptions.py
@@ -3,3 +3,7 @@
 class ScheduleError(LookupError):
     """Exception raised for errors in extracting schedule data."""
     pass
+
+class UploadError(ConnectionError):
+    """Exception raised for errors related to API uploads."""
+    pass
