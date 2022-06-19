5e40b41650ce51b1016eca855f9a7f1b12e8a7a4
studybuffalo <studybuffalo@gmail.com>
2022-06-19 10:27:39 -0600

    Update retrieve module to find most recent version of schedule

--- modules/retrieve.py
+++ modules/retrieve.py
@@ -6,21 +6,31 @@ from pathlib import Path
 LOG = logging.getLogger(__name__)
 
 
+def _get_most_recent_file(directory, glob_statement):
+    """Returns the most recent file from list of files."""
+    files = directory.glob(glob_statement)
+
+    try:
+        return max(files, key=lambda file: file.stat().st_mtime)
+    except ValueError:
+        raise FileNotFoundError(f'Unable to find file for glob: "{glob_statement}".')
+
+
 def retrieve_schedule_file_paths(config):
     """Creates the path to the schedules from supplied config file."""
     schedule_loc = Path(config['excel']['schedule_loc'])
 
     # Get the most recent assistant file
-    assistant_files = schedule_loc.glob(f'assistant_*.{config["excel"]["ext_a"]}')
-    assistant_latest = max(assistant_files, key=lambda x: x.stat().st_ctime)
+    assistant_glob = f'assistant_*.{config["excel"]["ext_a"]}'
+    assistant_latest = _get_most_recent_file(schedule_loc, assistant_glob)
 
     # Get the most recent pharmacist file
-    pharmacist_files = schedule_loc.glob(f'pharmacist_*.{config["excel"]["ext_p"]}')
-    pharmacist_latest = max(pharmacist_files, key=lambda x: x.stat().st_ctime)
+    pharmacist_glob = f'pharmacist_*.{config["excel"]["ext_p"]}'
+    pharmacist_latest = _get_most_recent_file(schedule_loc, pharmacist_glob)
 
     # Get the most recent technician file
-    technician_files = schedule_loc.glob(f'technician_*.{config["excel"]["ext_t"]}')
-    technician_latest = max(technician_files, key=lambda x: x.stat().st_ctime)
+    technician_glob = f'technician_*.{config["excel"]["ext_t"]}'
+    technician_latest = _get_most_recent_file(schedule_loc, technician_glob)
 
     # Return the final details
     return {
--- tests/test__retrieve.py
+++ tests/test__retrieve.py
@@ -1,1 +1,83 @@
 """Tests for the retrieve module."""
+import os
+from pathlib import Path
+from unittest.mock import patch
+
+from modules import retrieve
+
+
+def test__get_most_recent_file__one_file():
+    """Tests expected handling when one file available."""
+    # Get reference to test file directory
+    current_dir = Path(__file__).parent.absolute()
+    directory = Path(current_dir, 'files')
+
+    # Setup glob statement
+    glob_statement = 'example_*.xls'
+
+    file = retrieve._get_most_recent_file(directory, glob_statement)
+
+    assert 'example_xls.xls' in str(file)
+
+
+def test__get_most_recent_file__multiple_files():
+    """Tests expected handling when multiple files available."""
+    # Get reference to test file directory
+    current_dir = Path(__file__).parent.absolute()
+    directory = Path(current_dir, 'files')
+
+    # Get reference to test files
+    file_1 = directory / 'example_xlsx 1.xlsx'
+    file_2 = directory / 'example_xlsx 2.xlsx'
+    file_3 = directory / 'example_xlsx 3.xlsx'
+
+    # Manipulate modified times
+    os.utime(file_1, (10, 100))
+    os.utime(file_2, (20, 200))
+    os.utime(file_3, (15, 150))
+
+    # Setup glob statement
+    glob_statement = 'example_xlsx*.xlsx'
+
+    file = retrieve._get_most_recent_file(directory, glob_statement)
+
+    assert 'example_xlsx 2.xlsx' in str(file)
+
+def test__get_most_recent_file__no_files():
+    """Tests expected handling when no files available."""
+    # Get reference to test file directory
+    current_dir = Path(__file__).parent.absolute()
+    directory = Path(current_dir, 'files')
+
+    # Setup glob statement
+    glob_statement = 'incorrect_example*.xlsx'
+
+    try:
+        retrieve._get_most_recent_file(directory, glob_statement)
+    except FileNotFoundError as error:
+        assert 'Unable to find file for glob: "incorrect_example*.xlsx".' in str(error)
+    else:
+        assert False
+
+
+@patch('modules.retrieve._get_most_recent_file', lambda x, y: f'{x}_{y}')
+def test__retrieve_schedule_file_paths():
+    """Tests for exected processing and results in function."""
+    config = {
+        'excel': {
+            'schedule_loc': 'A',
+            'ext_a': 'B',
+            'ext_p': 'C',
+            'ext_t': 'D',
+        }
+    }
+
+    details = retrieve.retrieve_schedule_file_paths(config)
+
+    assert isinstance(details, dict)
+    assert 'a' in details
+    assert 'A_assistant_*.B' in str(details['a'])
+    assert 'p' in details
+    assert 'A_pharmacist_*.C' in str(details['p'])
+    assert 't' in details
+    assert 'A_technician_*.D' in str(details['t'])
